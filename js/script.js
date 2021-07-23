184a2df1148cbee04b9835b893aaa18f0c813b00
oradocaj <oradocaj@gmail.com>
2021-07-23 23:57:07 +0200

    Added local files to remote repo

---
+++ js/script.js
@@ -0,0 +1,47 @@
+setTimeout(function() {
+	location.href = 'rocket.html';
+}, 3000);
+//this function does this.
+var doCoolStuff = function () {
+	var currentClasName = document.getElementById('cool').className;
+
+	if (currentClasName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+	//changing class name
+	
+}
+var sayMyName = function (name) {
+	alert('My name is: '+name);
+}
+
+var car = {
+			make: 'VW',
+			type: 'Polo',
+			color: 'blue',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: [
+			'seat1',
+			'seat2',
+			'seat3',
+			'seat4'
+			],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {
+				console.log('turn car' + isOn);
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				} else {
+					this.isTurnedOn = false;
+				}
+			}
+		};
+		console.log('hello there friends!');
